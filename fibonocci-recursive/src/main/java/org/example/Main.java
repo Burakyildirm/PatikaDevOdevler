c7b6751eb689c17a7b66bfed96ce08fb6b83e2d6
BURAK <127990924+Burakyildirm@users.noreply.github.com>
2023-07-02 11:24:22 +0300

    Add files via upload

---
+++ fibonocci-recursive/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Fibonacci serisi için sınır değeri girin: ");
+        int n = input.nextInt();
+
+        System.out.println("Fibonacci serisi:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
