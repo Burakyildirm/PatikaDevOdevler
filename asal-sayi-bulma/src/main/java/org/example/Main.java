bc686138cb5ae18503aadfb8a11dc0a60a8297b3
BURAK <127990924+Burakyildirm@users.noreply.github.com>
2023-07-02 12:34:29 +0300

    Add files via upload

---
+++ asal-sayi-bulma/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Asal Sayılar: ");
+
+        for (int i = 2; i <= 100; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
