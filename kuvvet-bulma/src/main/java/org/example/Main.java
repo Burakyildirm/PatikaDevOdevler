b38c05ef619f98aed6d8e58a92f1b7ce6c493e93
BURAK <127990924+Burakyildirm@users.noreply.github.com>
2023-07-02 11:15:33 +0300

    Add files via upload

---
+++ kuvvet-bulma/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int num = scanner.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            System.out.println("4^" + i + " = " + Math.pow(4, i));
+            System.out.println("5^" + i + " = " + Math.pow(5, i));
+        }
+    }
+}
